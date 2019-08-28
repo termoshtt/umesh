71a82b88a886fa80ff87de5be53ed25b742269f0
Toshiki Teramura <toshiki.teramura@gmail.com>
2019-08-28 15:41:37 +0900

    Module doc

--- src/half_edge.rs
+++ src/half_edge.rs
@@ -1,5 +1,16 @@
+/*!
+
+Implementation of discrete differential geometrical (DDG) algorithms
+based on [Keenan Crane][DDG].
+
+[DDG]: https://www.cs.cmu.edu/~kmcrane/Projects/DDG
+
+*/
+
 #[derive(Debug, Clone)]
 struct Mesh {
+    /// A0 matrix in DDG
     vertex_edge: Vec<(usize, usize)>,
+    /// A1 matrix in DDG
     edge_face: Vec<(usize, usize)>,
 }
