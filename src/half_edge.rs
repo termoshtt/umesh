4fb243816bc957e750b09223a57cb2bafa389c11
Toshiki Teramura <toshiki.teramura@gmail.com>
2019-08-30 07:08:21 +0900

    Remove Vertex, Edge, Face because it will cause mutability problem

--- src/half_edge.rs
+++ src/half_edge.rs
@@ -10,10 +10,7 @@ based on [Keenan Crane][DDG].
 use crate::{connection_matrix::*, permutation::*};
 
 #[derive(Debug, Clone)]
-pub struct Mesh<Vertex, Edge, Face> {
-    vertices: Vec<Vertex>,
-    edges: Vec<Edge>,
-    faces: Vec<Face>,
+pub struct Mesh {
     /// A0 matrix in DDG
     vertex_edge: ConnectionMatrix,
     /// Transpose of A0
@@ -24,29 +21,18 @@ pub struct Mesh<Vertex, Edge, Face> {
     face_edge: ConnectionMatrix,
 }
 
-impl<Vertex, Edge, Face> Mesh<Vertex, Edge, Face> {
+impl Mesh {
     /// Create mesh from two connection matrices `A0` and `A1` in DDG
     ///
     /// Vertices, edges, and faces are initialized by `Default` trait.
-    pub fn from_connections(vertex_edge: ConnectionMatrix, edge_face: ConnectionMatrix) -> Self
-    where
-        Vertex: Default + Clone,
-        Edge: Default + Clone,
-        Face: Default + Clone,
-    {
-        let (v, e1) = vertex_edge.shape();
-        let (e2, f) = edge_face.shape();
+    pub fn from_connections(vertex_edge: ConnectionMatrix, edge_face: ConnectionMatrix) -> Self {
+        let (_, e1) = vertex_edge.shape();
+        let (e2, _) = edge_face.shape();
         assert_eq!(e1, e2); // Vertex-Edge matrix and Edge-Face matrix are compatible
-        let vertices = vec![Default::default(); v];
-        let edges = vec![Default::default(); e1];
-        let faces = vec![Default::default(); f];
 
         let edge_vertex = vertex_edge.transpose();
         let face_edge = edge_face.transpose();
         Mesh {
-            vertices,
-            edges,
-            faces,
             vertex_edge,
             edge_vertex,
             edge_face,
@@ -57,12 +43,7 @@ impl<Vertex, Edge, Face> Mesh<Vertex, Edge, Face> {
     /// Create from permutation (see DDG §2.5 for detail)
     ///
     /// Vertices, edges, and faces are initialized by `Default` trait.
-    pub fn from_permutation(permutation: &[usize]) -> Self
-    where
-        Vertex: Default + Clone,
-        Edge: Default + Clone,
-        Face: Default + Clone,
-    {
+    pub fn from_permutation(permutation: &[usize]) -> Self {
         let vertex_edge = ConnectionMatrix::from_iter(
             gather_vertices(permutation)
                 .iter()
